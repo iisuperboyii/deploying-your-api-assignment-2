c1b80acc19c0dad2d02036791bf8b397ba8d3c4d
iiuperboyii <aaronjomon24@gmail.com>
2025-01-23 10:35:59 +0530

    completed given tasks

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+const port = 3001;
 
 app.use(express.static('static'));
 
@@ -13,3 +14,24 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
+
+
+
+app.get('/', (req, res) => {
+    if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+    } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+    }
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000."));
